1630c6d7a0fff11430eb9b8ab6633e2c1e2f7117
Seonjin <ebngk0@gmail.com>
2025-03-17 14:36:28 +0900

    Design: 인기 LIVE 채널 컴포넌트 - 이전에 만든 카드 컴포넌트를 이용하여 구현

--- src/rename.py
+++ src/rename.py
@@ -1,13 +1,14 @@
 import os
 
 # 폴더 경로 설정
-folder_path = "./assets/poster/popular_programs"
+folder_path = "./assets/poster/LIVE"
 
 # 이미지 파일 가져오기
 image_files = [f for f in os.listdir(folder_path) if f.startswith("img_frame-") and f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
 # 이름 변경 규칙 설정
-name_map = {0: 'chaebol_son', 1: 'drunk_urban_women', 2: 'twilight', 3: 'Canada_checkin', 4: 'Saturday', 5: 'Show_me_the_money', 6: 'love_catcher', 7: 'useful_job'}
+# 영어로 하기 ㅠㅠ
+name_map = {0: 'JTBC', 1: 'tvN', 2: 'YTN', 3: 'JTBC2', 4: '연합뉴스TV', 5: '투니버스'}
 size_map = {0: '_sm', 1: '_md', 2: '_lg'}
 
 for image in image_files:
@@ -15,7 +16,7 @@ for image in image_files:
     
     try:
         num = int(name.replace("img_frame-", ""))  # 숫자 추출
-        new_name = name_map[num % 8] + size_map[num // 8] + ext  # 새로운 이름 생성
+        new_name = name_map[num % 6] + size_map[num // 6] + ext  # 새로운 이름 생성
         old_path = os.path.join(folder_path, image)
         new_path = os.path.join(folder_path, new_name)
         os.rename(old_path, new_path)  # 파일 이름 변경
