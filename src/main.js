09333f0d0a223c313fb1c4b6d26c556e15484293
Estacionales <kebihoya@naver.com>
2025-03-20 14:13:14 +0900

    refactor: login_page
    - ARIA가 필요한 곳에 역할 부여.
    - 간단한 CSS 애니메이션 적용.

--- src/main.js
+++ src/main.js
@@ -61,15 +61,16 @@ document.addEventListener('DOMContentLoaded', () => {
       button.addEventListener('click', closeDialog);
     });
   }
+  // 로그아웃 모달
+  // DOM 선택 부분
+  const showDialogButton = document.querySelector('.show-dialog');
+  const closeDialogButton = document.querySelector('.close-dialog');
+  const panelDialog = document.querySelector('.panel-dialog');
+  // 함수 구현 부분
+  const openDialog = () => panelDialog.showModal();
+  const closeDialog = () => panelDialog.close();
+  // 이벤트 바인딩 부분
+  showDialogButton.addEventListener('click', openDialog);
+  closeDialogButton.addEventListener('click', closeDialog);
 });
 
-// DOM 선택 부분
-const showDialogButton = document.querySelector('.show-dialog');
-const closeDialogButton = document.querySelector('.close-dialog');
-const panelDialog = document.querySelector('.panel-dialog');
-// 함수 구현 부분
-const openDialog = () => panelDialog.showModal();
-const closeDialog = () => panelDialog.close();
-// 이벤트 바인딩 부분
-showDialogButton.addEventListener('click', openDialog);
-closeDialogButton.addEventListener('click', closeDialog);
